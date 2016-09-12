272be42c7497c3eff4f8b5b1422940bcfb0178da
n3vrax <n3vrax@gmail.com>
2016-09-12 18:18:46 +0300

    removed dependency to dk-session and use zend session directly

---
+++ test/FlashMessengerTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: n3vrax
+ * Date: 9/12/2016
+ * Time: 6:12 PM
+ */
+
+namespace DotKernelTest\DotFlashMessenger;
+
+
+class FlashMessengerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSomething()
+    {
+
+    }
+}
